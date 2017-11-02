1429f8c39ebca35dc7898e9a29b61085f5e13700
alexsleat <alexsleat@gmail.com>
2017-11-02 16:57:03 +0000

    Partially complete callback function

--- tbm1_getting_to_know_my_home/scripts/know_my_home_controller.py
+++ tbm1_getting_to_know_my_home/scripts/know_my_home_controller.py
@@ -8,8 +8,49 @@ from geometry_msgs.msg import Pose2D
 
 class Controller():
     def __init__(self):
+        
+        # Publishers - sends out goal locations, movement/turns, and speech
+        
         self.pub_task = rospy.Publisher('hearts/controller/task', String, queue_size=10)
+        #self.pubGoal = rospy.Publisher('/hearts/navigation/goal', PoseStamped, queue_size=10)
+        self.pub_talk = rospy.Publisher('/hearts/tts', String, queue_size = 10)
 
-        #subscribers
-
-
+        # Subscribers - must listen for speech, location
+        rospy.Subscriber("hearts/navigation/goal/location", String, self.locGoal_callback)
+        rospy.Subscriber("/hearts/stt", String, self.hearCommand_callback)
+        self.items = ['thing1','thing2','etc']
+        self.tbm1_commands_dict = {
+            "move": ["forward", "backward"],
+            "turn": ["right", "left"],
+            "go": ["kitchen", "bedroom", "living", "dining", "family", "room"],
+            "look": ["up", "down", "right", "left"],
+            "see": self.items,
+            "changed": self.items,
+            "stop": []}
+            
+        
+    def hearCommand_callback(self, data):
+        s = data
+        words = s.split(' ')
+        global verb
+        global subject
+        possible_verbs = self.tbm1_commands_dict.keys()
+        to_remove = ['to','the']
+        #verb = ''
+        #subject = ''
+        for command in possible_verbs:
+            if command in words:
+                valid_command = True
+                verb = command
+                for r in to_remove:
+                    if r in words:
+                        words.remove(word)
+                
+                subject = " ".join(words)
+            else:
+                valid_command = False
+        print('original',s)
+        print('verb',verb)
+        print('subject',subject)
+        return 
+        
