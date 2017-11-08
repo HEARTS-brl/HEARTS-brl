8dc0f0508532fbc01c8b73ce2434eb0a6df20182
Unknown <mail@zekesteer.me.uk>
2017-11-08 16:36:34 +0000

    camera saver for TBM1

---
+++ hearts_camera_saver/scripts/save_cam_images.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridgeError
+from os import path
+import cv2
+import numpy as np
+
+class CameraSaver:
+        
+    def __init__(self, input_topic, output_path):
+        self.output_path = output_path
+        rospy.Subscriber(input_topic, CompressedImage, self.image_cb)
+        rospy.Subscriber("/hearts/camera/snapshot", String, self.filename_cb)
+        
+    def image_cb(self, msg):
+        try:
+            np_arr = np.fromstring(msg.data, np.uint8)
+            self.image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+
+        except CvBridgeError, e:
+            print(e)
+            
+    def filename_cb(self, msg):
+        filepath = path.join(self.output_path, msg.data)
+        if self.image is None:
+            print("no camera image to save at " + filepath)
+        else:
+            cv2.imwrite(filepath, self.image)
+            print("saved camera image at " + filepath)
+            
+def main():
+    rospy.init_node('camera_saver')
+    
+    input_topic = rospy.get_param("input_topic")
+    output_path = rospy.get_param("output_path")
+    camera_saver = CameraSaver(input_topic, output_path)
+    
+    print("Camera saver running...")
+    
+    rospy.spin()
+    
+if __name__ == '__main__':
+    main()
